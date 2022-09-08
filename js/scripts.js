f5f696808316773d314320ea9bdc51f561bab1aa
krystyna0 <krystyna3541@ukr.net>
2022-09-09 00:25:24 +0200

    processed

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 $(document).ready(function(){
 
-    $('.owl-carousel').owlCarousel({
+    $('.slide-one').owlCarousel({
         loop:true,
         margin:65,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:3
+            }
+        }
+    });
+
+    $('.slide-two').owlCarousel({
+        loop:true,
+        margin:33,
         nav:true,
         navText: [ '', ' ' ],
 

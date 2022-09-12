9fbb7287303b719ebf7e9c2e8f0021187adeeaa1
krystyna0 <krystyna3541@ukr.net>
2022-09-12 18:14:17 +0200

    test adaptation

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,8 @@ $(document).ready(function(){
 
         responsive:{
             0:{
-                items:1
+                items:1,
+                margin:0,
             },
 
             1000:{
